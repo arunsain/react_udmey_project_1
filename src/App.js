740e40c6fc2b7ec6f111a17f7b280f1f285931be
arunsain3 <arunsain3@gmail.com>
2022-05-20 22:35:50 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 
 import './App.css';
 
-import Expenses from './components/Expenses';
+import Expenses from './components/Expenses/Expenses';
 function App() {
-  
+  const expenses = [
+    {
+      title:"car insurance",
+      amount: 234,
+      date: new Date(2022 , 4 ,18)
+    },
+    {
+      title:"toliet paper",
+      amount: 256,
+      date: new Date(2022 , 6 ,25)
+    },
+    {
+      title:"paper soap",
+      amount: 247,
+      date: new Date(2022 , 4 ,7)
+    },
+    {
+      title:"colgate",
+      amount: 333,
+      date: new Date(2022 , 5 ,11)
+    }
+  ]
  
   return (
     <div className="App">
-      <Expenses></Expenses>
+      <Expenses items={expenses}></Expenses>
     </div>
   );
 }
